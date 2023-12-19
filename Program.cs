9d5a29ad85e88f3f7a1b5d4935ab169b1719ebd0
XhRome85 <151630406+XhRome85@users.noreply.github.com>
2023-12-19 11:08:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
 ﻿
-int[] nums = new int[10];
-for (int i = 0; i < nums.Length; i++)
-{
-    nums[i] = new Random().Next(1, 101);
-    Console.Write(nums[i] + " ");
-}
-int count = 0;
-for (int i = 0; i < nums.Length; i++)
-{
-    if (nums[i] >= 20 && nums[i] <= 90);
-    {
-        count++;
-    }
+// int[] nums = new int[10];
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     nums[i] = new Random().Next(1, 101);
+//     Console.Write(nums[i] + " ");
+// }
+// int count = 0;
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     if (nums[i] >= 20 && nums[i] <= 90);
+//     {
+//         count++;
+//     }
 
-}
-Console.WriteLine();
-Console.WriteLine(count);
+// }
+// Console.WriteLine();
+// Console.WriteLine(count);
 
+// -----------------------------------------------------------------
 
+// int[] nums = new int[10];
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     nums[i] = new Random().Next(1, 41);
+//     Console.Write(nums[i]+ ", ");
+// }
+// int count = 0;
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     if (nums[i] % 2 == 0)
+//     {
+//         count++;
+//     }
+
+// }
+// Console.WriteLine();
+// Console.WriteLine(+count);
+
+// -----------------------------------------------------------------
