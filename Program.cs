cbd4bec762e49f93a185a44c699068300b4676aa
XhRome85 <151630406+XhRome85@users.noreply.github.com>
2023-12-19 13:10:33 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@
 // for (int i = 0; i < nums.Length; i++)
 // {
 //     nums[i] = new Random().Next(1, 101);
-//     Console.Write(nums[i] + " ");
+//     Console.Write(nums[i] + "|");
 // }
 // int count = 0;
 // for (int i = 0; i < nums.Length; i++)
 // {
-//     if (nums[i] >= 20 && nums[i] <= 90);
+//     if (nums[i] >= 20 && nums[i] <= 90)
 //     {
 //         count++;
 //     }
@@ -38,3 +38,33 @@
 // Console.WriteLine(+count);
 
 // -----------------------------------------------------------------
+
+double[] array = new double[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble();
+    string result = string.Format("{0:0.00}", array[i]);
+    Console.Write(result + " | ");
+}
+double maxElement = array.Max();
+double minElement = array.Min();
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > maxElement)
+    {
+        maxElement = array[i];
+    }
+    if (array[i] > minElement)
+    {
+        minElement = array[i];
+    }
+}
+double razn = maxElement - minElement;
+string result_2 = string.Format("{0:0.00}", razn);
+Console.WriteLine();
+Console.WriteLine("Разность " + result_2);
+
+
+
+
+
