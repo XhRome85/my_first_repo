c5cedda85708ec7c3fe62fd662a94961a75c86ed
XhRome85 <151630406+XhRome85@users.noreply.github.com>
2023-12-19 10:26:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] nums = new int[10];
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = new Random().Next(1, 101);
+    Console.Write(nums[i] + " ");
+}
+int count = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] >= 20 && nums[i] <= 90);
+    {
+        count++;
+    }
+
+}
+Console.WriteLine();
+Console.WriteLine(count);
+
+
